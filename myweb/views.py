609b79030b65a92799c73b130082afc88a2e2370
umairmalik24023 <umairmalik24023@gmail.com>
2023-02-24 19:48:31 +0500

    My First Commit

---
+++ myweb/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+from .import counter
+
+def home(request):
+    
+    return render(request, 'index.html',{'key1': 'I am coming from python code'})
+ 
+def result(request):
+    article= request.GET['article']
+    var_dict,word_count= counter.count(article)
+    return render(request, 'result.html', {'article': article, 'word_count': word_count, 'dict_words':var_dict})               
+                  
+
+# def downloads(request):
+#     return HttpResponse('<h1>No Downloads</h1>')
